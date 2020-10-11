d3f0a36501e9b8b83d624fe9874913efe106e28f
Nuno Ferreira <nuno.ferreira@intellilift.no>
2020-10-11 18:11:25 +0200

    Updates

---
+++ precache-manifest.149700a894ef12cf53649a974e675b45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52666fe8c1703f66a5e8d268e6e503a8",
+    "url": "/parking-garage/index.html"
+  },
+  {
+    "revision": "b0296f64c5efc90fb661",
+    "url": "/parking-garage/static/css/main.d2eaa143.chunk.css"
+  },
+  {
+    "revision": "d45488670ebff652498c",
+    "url": "/parking-garage/static/js/2.ade2e5c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/parking-garage/static/js/2.ade2e5c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0296f64c5efc90fb661",
+    "url": "/parking-garage/static/js/main.2f76aabd.chunk.js"
+  },
+  {
+    "revision": "e24a2bbf606cec9b8bd6",
+    "url": "/parking-garage/static/js/runtime-main.596517a6.js"
+  },
+  {
+    "revision": "2135ece3974b8870d7802e2f56711ea7",
+    "url": "/parking-garage/static/media/bg.2135ece3.jpg"
+  }
+]);
